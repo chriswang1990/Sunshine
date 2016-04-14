573542367b8e92042de4d24be489f095c46e5d7f
chriswang1990 <1990chriswang1990@gmail.com>
2016-04-14 01:25:04 -0700

    Add forecast data and adapter, link them to the listview for forecast list display

--- app/src/main/java/com/upenn/chriswang1990/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/upenn/chriswang1990/sunshine/MainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.upenn.chriswang1990.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +21,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> forecastData= new ArrayList<>();
+        forecastData.add("Today - Sunny - 20/10");
+        forecastData.add("Tomorrow - Rainy - 15/9");
+        forecastData.add("Wednesday - Sunny - 22/10");
+        forecastData.add("Thursday - Cloudy - 15/8");
+        forecastData.add("Friday - Sunny - 23/13");
+        forecastData.add("Saturday - Sunny - 26/16");
+        forecastData.add("Sunny - Sunny - 25/15");
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<>(getActivity(), R.layout
+                .list_item_forecast, R.id.list_item_forecast_textview, forecastData);
+        ListView forecastList = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastList.setAdapter(forecastAdapter);
+        return rootView;
     }
 }
