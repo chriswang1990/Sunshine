d07ce236ebe43d2452086d0e5b42978a18ab018b
chriswang1990 <1990chriswang1990@gmail.com>
2016-04-18 19:58:54 -0700

    Create intent in MainActivity for SettingActivity

--- app/src/main/java/com/upenn/chriswang1990/sunshine/MainActivity.java
+++ app/src/main/java/com/upenn/chriswang1990/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.upenn.chriswang1990.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -32,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/upenn/chriswang1990/sunshine/SettingsActivity.java
+++ app/src/main/java/com/upenn/chriswang1990/sunshine/SettingsActivity.java
@@ -8,7 +8,7 @@ import android.preference.PreferenceManager;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings.
- * <p>
+ * <p/>
  * See <a href="http://developer.android.com/design/patterns/settings.html">
  * Android Design: Settings</a> for design guidelines and the <a
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
