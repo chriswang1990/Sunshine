524050bf6ea85885f73db0af44ad68b9a31f70ab
chriswang1990 <1990chriswang1990@gmail.com>
2016-04-28 16:28:20 -0700

    Minor fix in matcher method and comments to make code clearer

--- app/src/androidTest/java/com/upenn/chriswang1990/sunshine/data/TestWeatherContract.java
+++ app/src/androidTest/java/com/upenn/chriswang1990/sunshine/data/TestWeatherContract.java
@@ -28,9 +28,6 @@ public class TestWeatherContract extends AndroidTestCase {
     private static final String TEST_WEATHER_LOCATION = "/North Pole";
     private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
 
-    /*
-        Students: Uncomment this out to test your weather location function.
-     */
     public void testBuildWeatherLocation() {
         Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
         assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
--- app/src/main/java/com/upenn/chriswang1990/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/upenn/chriswang1990/sunshine/data/WeatherProvider.java
@@ -109,24 +109,19 @@ public class WeatherProvider extends ContentProvider {
         );
     }
 
-    /*
-        Students: Here is where you need to create the UriMatcher. This UriMatcher will
-        match each URI to the WEATHER, WEATHER_WITH_LOCATION, WEATHER_WITH_LOCATION_AND_DATE,
-        and LOCATION integer constants defined above.  You can test this by uncommenting the
-        testUriMatcher test within TestUriMatcher.
-     */
     static UriMatcher buildUriMatcher() {
         // 1) The code passed into the constructor represents the code to return for the root
         // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
         UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority =WeatherContract.CONTENT_AUTHORITY;
         // 2) Use the addURI function to match each of the types.  Use the constants from
         // WeatherContract to help define the types to the UriMatcher.
-        matcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER, WEATHER);
-        matcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*",
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*",
               WEATHER_WITH_LOCATION);
-        matcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*/#",
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/#",
              WEATHER_WITH_LOCATION_AND_DATE);
-        matcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_LOCATION, LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
         // 3) Return the new matcher!
         return matcher;
     }
