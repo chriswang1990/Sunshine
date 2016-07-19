e1f19aba287a1982d88a58d7963d0eea9cda23ea
chriswang1990 <1990chriswang1990@gmail.com>
2016-07-18 23:12:11 -0700

    Disable the ok button when user enter less than minLength in location setting

--- app/src/main/java/com/upenn/chriswang1990/sunshine/LocationEditTextPreference.java
+++ app/src/main/java/com/upenn/chriswang1990/sunshine/LocationEditTextPreference.java
@@ -1,9 +1,16 @@
 package com.upenn.chriswang1990.sunshine;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LocationEditTextPreference extends EditTextPreference {
     private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
@@ -21,5 +28,39 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        EditText et = getEditText();
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Dialog d = getDialog();
+                if (d instanceof AlertDialog) {
+                    AlertDialog dialog = (AlertDialog) d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if the EditText is empty
+                    if (editable.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
     }
 }
